ea42af8cdf9c45d0557e576c26ca5e889504bb92
igor <igoratakhanov@gmail.com>
2019-11-11 16:05:10 -0500

    getModelYears() passing

--- index.js
+++ index.js
@@ -191,8 +191,12 @@ const sortCarInventory = (carArr) => carArr.sort((a, b) => b.car_model !== a.car
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(carArr) {
+  const yearArr = []
+  for (let i = 0 ; i < carArr.length; i++) {
+    yearArr.push(carArr[i].car_year) 
+  }
+  return yearArr
 }
 
 /**
